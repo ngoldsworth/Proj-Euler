6b6bc8249ad021f2f16d40f06fc12a8b448ad187
Nelson Goldsworth <nelson.goldsworth@gmail.com>
2018-08-13 11:36:14 -0600

    Initial commit

---
+++ is_prime.py
@@ -0,0 +1,18 @@
+import math
+
+def is_prime(n):
+    for j in range(3, n):
+        if n % j == 0:  # aritmetic calculations
+            return False
+    return True
+
+primes = []  # initialize a list of prines collected.
+x = 10001
+n = 2
+
+while len(primes) !=x+1: # is the nth number on the list yet? +1 because array starts at zero
+    if is_prime(n):
+        primes.append(n) # adds that prime number to the listi
+    n+=1 # increment to the next number
+
+print(primes[-1])
