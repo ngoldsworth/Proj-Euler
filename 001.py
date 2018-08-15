dbb4f9846939c2b4982ecf0dbde6afe14a284c41
Nelson Goldsworth <nelson.goldsworth@gmail.com>
2018-08-15 15:55:02 -0600

    Updating 001

--- 001.py
+++ 001.py
@@ -1,12 +1,28 @@
-import math
+numArray = []
+a = 0
+b = 0
+total = 0
+totala = 0
+totalb = 0
 
-x = 1000
+# numArray a and b are used to test the array
+# is the right length
+numArraya = []
+numArrayb = []
 
-dthree = int(math.floor(x/3))
-dfive = int(math.floor(x/5))
+while a < 1000:
+    numArray.append(a)
+    numArraya.append(a)
+    a += 3
+# This above section will add every third integer to the array.
 
-three_sum = int(3*dthree*(dthree+1)*0.5)
-five_sum = int(5*dfive(dfive+1)*0.5)
+while b < 1000:
+    if b not in numArray:
+        numArray.append(b)
+        numArrayb.append(b)
+    b += 5
 
-final_sum = three_sum + five_sum
-print final_sum
+for num in numArray:
+    total += num
+print (total)
+
