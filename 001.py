b748cd59dd1ab595dd97d521b9748f88038ac318
Nelson Goldsworth <nelson.goldsworth@gmail.com>
2018-08-14 23:12:07 -0600

    Updating 001

--- 001.py
+++ 001.py
@@ -2,10 +2,11 @@ import math
 
 x = 1000
 
-d3 = int(math.floor(x/3))
-d5 = int(math.floor(x/5))
+dthree = int(math.floor(x/3))
+dfive = int(math.floor(x/5))
 
-three_sum = int(3*d3*(d3+1)*0.5)
-five_sum = int(5*d5(d5+1)*0.5)
+three_sum = int(3*dthree*(dthree+1)*0.5)
+five_sum = int(5*dfive(dfive+1)*0.5)
 
 final_sum = three_sum + five_sum
+print final_sum
