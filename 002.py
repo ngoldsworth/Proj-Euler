c46ed4a688835f5ab783226d99b48854c553aa0b
Nelson Goldsworth <nelson.goldsworth@gmail.com>
2018-08-16 01:07:56 -0600

    removing uneccesary comments

--- 002.py
+++ 002.py
@@ -4,9 +4,6 @@ while x < 4000000:
     f1=fib[-1]
     f2=fib[-2]
     x = f1 + f2
-#    print(x)
     fib.append(x)
-# if x % 2 != 0 and x <= 4000000:
-
 SUM = sum(num for num in fib if num % 2 != 0)
 print(SUM)
