90bf8addb6187e0550b123f9c693817fae93ab60
Nelson Goldsworth <nelson.goldsworth@gmail.com>
2019-08-13 15:02:05 -0600

    Some stuff getting committed after last week

---
+++ p035.py
@@ -0,0 +1,12 @@
+from primes.primelist import PrimesList, primesfrom2to
+
+def circle_number(num: int):
+    digilist = list(str(num))
+    circled = [num]
+    for j in range(len(digilist)):
+
+
+
+
+max_num = 10**6 + 1
+plist = PrimesList(max_num)
